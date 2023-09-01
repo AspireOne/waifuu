8677a13386177c83d0682d33106af3fb7c94b054
Matěj Pešl <matejpesl1@gmail.com>
2023-09-01 20:35:04 +0200

    force-push to re-trigged deploy

--- src/server/db.ts
+++ src/server/db.ts
@@ -11,5 +11,5 @@ export const prisma =
     log:
       process.env.NODE_ENV === "development" ? ["query", "error", "warn"] : ["error"],
   });
-
+ 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
