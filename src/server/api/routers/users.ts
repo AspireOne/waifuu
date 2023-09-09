f9533df5d2c68e87942525d2c268e643f24c0103
Matěj Pešl <matejpesl1@gmail.com>
2023-09-09 22:27:35 +0200

    Add user routes to API
    Added new 'users.ts' router to the server API. This provides two endpoints, 'getSelf', which returns data for the logged-in user, and 'getPublic', which returns publically available data for any user given their username. The addition of

---
+++ src/server/api/routers/users.ts
@@ -0,0 +1,50 @@
+import {
+  createTRPCRouter,
+  protectedProcedure,
+  publicProcedure,
+} from "~/server/api/trpc";
+import { z } from "zod";
+
+export const usersRouter = createTRPCRouter({
+  getSelf: protectedProcedure.query(async ({ ctx }) => {
+    return await ctx.prisma.user.findUnique({
+      where: {
+        id: ctx.session.user.id,
+      },
+      include: {
+        Bot: true,
+      },
+    });
+  }),
+
+  getPublic: publicProcedure
+    .input(
+      z.object({
+        username: z.string(),
+      }),
+    )
+    .query(async ({ input, ctx }) => {
+      const user = await ctx.prisma.user.findUnique({
+        where: {
+          username: input.username,
+        },
+        include: {
+          Bot: {
+            where: {
+              visibility: "PUBLIC",
+            },
+          },
+        },
+      });
+
+      if (!user) return undefined;
+
+      // Hand-pick the returned properties, so we don't leak sensitive information.
+      return {
+        id: user.id,
+        name: user.name,
+        image: user.image,
+        bots: user.Bot,
+      };
+    }),
+});
