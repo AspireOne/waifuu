454ec3a30e19cea2d3e5fce218317c066df76874
Matěj Pešl <matejpesl1@gmail.com>
2023-09-10 12:43:08 +0200

    add brand background color

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,5 @@
 import { type Config } from "tailwindcss";
+
 const { nextui } = require("@nextui-org/react");
 
 export default {
@@ -9,7 +10,9 @@ export default {
   ],
   theme: {
     extend: {
-      colors: {},
+      colors: {
+        brandBg: "#1E1E1E",
+      },
     },
   },
   /*darkMode: "class",*/
