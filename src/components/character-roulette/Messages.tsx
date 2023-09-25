28be17de8b78849c90c73100bfe181ce12910d2a
Matěj Pešl <matejpesl1@gmail.com>
2023-09-25 11:28:59 +0200

    move markdown function to utils

--- src/components/character-roulette/Messages.tsx
+++ src/components/character-roulette/Messages.tsx
@@ -2,6 +2,7 @@ import { RRMessage, RRMessages, RRSystemMessage } from "~/hooks/useRRMessages";
 import { Card, CardBody, CardHeader } from "@nextui-org/card";
 import React from "react";
 import { Avatar } from "@nextui-org/avatar";
+import { applyMarkdown } from "~/utils/utils";
 
 export default function RRMessages(props: { messages: RRMessages }) {
   if (props.messages.length === 0) return undefined;
@@ -43,40 +44,3 @@ const SystemMessage = (props: { message: RRSystemMessage }) => (
     </CardBody>
   </Card>
 );
-
-/**
- * Takes in a string and applies markdown: *italic*, **bold**, `code`.
- * @example "Hello *world*! I am **Kate**" -> ["Hello ", <i>world</i>, "!", " I am ", <b>Kate</b>]
- */
-export function applyMarkdown(text: string): React.ReactNode[] {
-  let result: (string | React.ReactNode)[] = [];
-
-  if (!text) return result;
-
-  // Regular expression to match *, **, ` and the text between them
-  let regex = /(\*\*[^*]*\*\*)|(\*[^*]*\*)|(`[^`]*`)/g;
-  let splits = text.split(regex);
-
-  for (let split of splits) {
-    if (!split) continue;
-
-    if (split.startsWith("**") && split.endsWith("**")) {
-      result.push(<b key={split}>{split.slice(2, -2)}</b>);
-    } else if (split.startsWith("*") && split.endsWith("*")) {
-      result.push(
-        <i className={"text-foreground-500"} key={split}>
-          {"\n"}
-          {split.slice(1, -1)}
-          {"\n"}
-        </i>,
-      );
-    } else if (split.startsWith("`") && split.endsWith("`")) {
-      result.push(<code key={split}>{split.slice(1, -1)}</code>);
-    } else {
-      // Keep the string as it is if it's not surrounded by special characters
-      result.push(split);
-    }
-  }
-
-  return result;
-}
