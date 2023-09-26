7be40148a009251f4ef5207bda33c28faa011c41
Matěj Pešl <matejpesl1@gmail.com>
2023-09-26 16:25:45 +0200

    add landing page placeholder

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,9 +2,16 @@ import React from "react";
 import Page from "~/components/Page";
 
 export default function LandingPage() {
+  // Todo: meta description.
   return (
-    <Page metaTitle={"Main Page"} unprotected>
-      <div></div>
+    <Page
+      metaTitle={"Companion"}
+      unprotected
+      header={{ enabled: false }}
+      showMobileNav={false}
+    >
+      <div>Here will be landing page</div>
+      <button>Click here to get to app homepage</button>
     </Page>
   );
 }
