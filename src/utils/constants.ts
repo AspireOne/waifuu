80246a603b3b79a1a208ee55c9703eb5a3012ab4
Matěj Pešl <matejpesl1@gmail.com>
2023-09-29 19:57:59 +0200

    refactor

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,13 +1,12 @@
 import { Capacitor } from "@capacitor/core";
 
+export class Constants {
+  static readonly SERVER_HOST_URL = "https://companion-red.vercel.app";
+}
+
 export const getBaseServerUrl = () => {
-  if (Capacitor.isNativePlatform()) {
-    const serverUrl = "https://companion-red.vercel.app";
-    console.log(
-      `Native project, using hardcoded external hosted server url (${serverUrl}).`,
-    );
-    return serverUrl; // TODO: Change this when we have a real domain.
-  }
+  console.log("is native platform: ", Capacitor.isNativePlatform());
+  if (Capacitor.isNativePlatform()) return Constants.SERVER_HOST_URL;
 
   if (typeof window !== "undefined") return ""; // Browser should use relative url.
 
@@ -15,9 +14,8 @@ export const getBaseServerUrl = () => {
   // prettier-ignore
   if (process.env.RAILWAY_PUBLIC_DOMAIN) return `https://${process.env.RAILWAY_PUBLIC_DOMAIN}`;
 
+  // TODO: Implement this on our server.
   if (process.env.DOMAIN) return `https://${process.env.PUBLIC_DOMAIN}`;
 
   return `http://localhost:${process.env.PORT ?? 3000}`; // Dev SSR should use localhost.
 };
-
-export const constants = {};
