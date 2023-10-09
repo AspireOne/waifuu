ec52223ee3ea0c845c090666f0f3a8c794e0615f
Jakub Dorničák <jakub.dornicak@blindspot.ai>
2023-10-09 12:39:54 +0200

    feat: 🎸 Forum psot

--- src/components/Forum/ForumPostComment.tsx
+++ src/components/Forum/ForumPostComment.tsx
@@ -9,35 +9,30 @@ type ForumPostProps = ForumPost & {
 
 export const ForumPostComment = (data: ForumPostProps) => {
   return (
-    <Card className="p-2">
-      <div className="flex flex-row gap-2">
-        <Image
-          src={data.author.image ?? "/default-avatar.png"}
-          width={50}
-          className="rounded-full"
-          height={50}
-          alt="Avatar"
-        />
+    <Card className="p-4">
+      <div className="flex flex-row gap-5">
+        <div className="flex flex-row gap-2">
+          <Image
+            src={data.author.image ?? "/default-avatar.png"}
+            width={50}
+            className="rounded-full"
+            height={50}
+            alt="Avatar"
+          />
 
-        <div className="flex flex-col spacing-2">
-          <p>dlkjfkd</p>
-          <p className="text-gray-400">@lkjfdlkfjd</p>
+          <div className="flex flex-col spacing-2">
+            <p>dlkjfkd</p>
+            <p className="text-gray-400">@lkjfdlkfjd</p>
+          </div>
+        </div>
+
+        <div className="flex flex-row gap-4 mx-auto mr-4 mt-2.5">
+          <FaHeart className="text-gray-500" fontSize={20} />
+          <FaComment className="text-primary-500" fontSize={20} />
         </div>
       </div>
 
       <p className="mt-3 mb-3">{data.content}</p>
-
-      <Flex orientation="row" gap="4">
-        <Flex orientation="row" className="text-center">
-            <FaHeart fontSize={20} />
-            <p>{parseInt(data.likeCount.toString())}</p>
-        </Flex>
-
-        <Flex orientation="row" className="text-center">
-            <FaComment fontSize={20} />
-            <p>{parseInt(data.likeCount.toString())}</p>
-        </Flex>
-      </Flex>
     </Card>
   );
 };
--- src/pages/forum/[id].tsx
+++ src/pages/forum/[id].tsx
@@ -1,8 +1,17 @@
-import { Button, Image, Textarea } from "@nextui-org/react";
+import {
+  Button,
+  Image,
+  Modal,
+  ModalContent,
+  Textarea,
+} from "@nextui-org/react";
 import { useRouter } from "next/router";
+import { useState } from "react";
 import { useForm } from "react-hook-form";
+import { FaHeart, FaReply } from "react-icons/fa";
 import { ForumPostComment } from "~/components/Forum/ForumPostComment";
 import Page from "~/components/Page";
+import { Flex } from "~/components/shared/Flex";
 import { api } from "~/utils/api";
 
 type CreateFormPostForm = {
@@ -11,6 +20,8 @@ type CreateFormPostForm = {
 
 export default function ForumPostPage() {
   const { id } = useRouter().query;
+
+  const [commentInputOpen, setCommentInputOpen] = useState(false);
 
   const { register, handleSubmit } = useForm<CreateFormPostForm>();
   const createCommentMutation = api.forum.comment.useMutation();
@@ -21,10 +32,14 @@ export default function ForumPostPage() {
     });
 
     postComments.refetch();
+    setCommentInputOpen(false);
   };
 
   const post = api.forum.get.useQuery({ id: id as string });
-  const postComments = api.forum.getPostComments.useQuery({ id: id as string, cursor: 0 });
+  const postComments = api.forum.getPostComments.useQuery({
+    id: id as string,
+    cursor: 0,
+  });
 
   return (
     <Page
@@ -41,23 +56,54 @@ export default function ForumPostPage() {
         />
 
         <div className="mt-2">
-          <h1 className="text-2xl font-bold">{post.data?.title}</h1>
-          <p>{post.data?.content}</p>
+          <Flex orientation="col" className="align-center">
+            <h1 className="text-2xl font-bold">{post.data?.title}</h1>
+            <p className="text-gray-500">33 minutes ago</p>
+          </Flex>
+
+          <p className="mt-3">{post.data?.content}</p>
+        </div>
+
+        <div className="mt-2 flex flex-row gap-2">
+          <Button color="danger">
+            <FaHeart />
+          </Button>
+
+          <Button onClick={() => setCommentInputOpen(true)} color="primary">
+            <FaReply />
+            Comment
+          </Button>
         </div>
       </header>
 
       <section className="flex flex-col gap-2">
-        <h1>Comments</h1>
+        <div className="flex flex-row gap-2 align-center mt-4">
+          <h1 className="text-xl font-bold">Comments</h1>
+          <label className="mt-0.5 text-gray-500">{postComments.data?.length} replies</label>
+        </div>
 
-        <form className="flex flex-col gap-2" onSubmit={handleSubmit(onSubmit)}>
-          <Textarea {...register("content")} placeholder="Comment" />
-          <Button isLoading={createCommentMutation.isLoading} type="submit">
-            Submit
-          </Button>
-        </form>
+        <Modal
+          isOpen={commentInputOpen}
+          onClose={() => setCommentInputOpen(false)}
+        >
+          <ModalContent className="p-3">
+            <h1 className="text-lg font-bold">Create a new comment</h1>
+            <form
+              className="flex flex-col gap-2"
+              onSubmit={handleSubmit(onSubmit)}
+            >
+              <Textarea {...register("content")} placeholder="Comment" />
+              <Button isLoading={createCommentMutation.isLoading} type="submit">
+                Submit
+              </Button>
+            </form>
+          </ModalContent>
+        </Modal>
 
         <div className="flex flex-col gap-6 p-2">
-            {postComments.data?.map((comment) => <ForumPostComment {...comment} />)}
+          {postComments.data?.map((comment) => (
+            <ForumPostComment {...comment} />
+          ))}
         </div>
       </section>
     </Page>
