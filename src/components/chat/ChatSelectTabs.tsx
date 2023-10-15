7574b6f22806e76be65ea83134370f11e7bca138
Jakub Dornicak <dornicakkuba@gmail.com>
2023-10-15 16:10:08 +0200

    feat: 🎸 Docker working

---
+++ src/components/chat/ChatSelectTabs.tsx
@@ -0,0 +1,64 @@
+import { Button, Card, CardBody, Tab, Tabs } from "@nextui-org/react";
+import { BotMode } from "@prisma/client";
+
+const tabs = [
+  {
+    title: "Adventure",
+    key: BotMode.ADVENTURE,
+    description:
+      "(i) Your world, your story. You control the character, and you decide how the story will unfold.",
+  },
+  {
+    title: "Chat",
+    key: BotMode.CHAT,
+    description: "(i) Casually chat with the character. Hey, how r u?",
+  },
+  {
+    title: "Roleplay",
+    key: BotMode.ROLEPLAY,
+    description: "(i) Talk with the character as if you were there with them.",
+  },
+];
+
+const ChatSelectTabs = ({
+  onSelect,
+  isLoading,
+}: {
+  onSelect: (data: BotMode) => void;
+  isLoading: boolean;
+}) => {
+  // TODO: FIX - Not working on firefox devtools,
+  return (
+    <div className="flex z-40 w-full flex-col">
+      <Tabs
+        color="primary"
+        size="lg"
+        className="mx-auto w-fit mb-4"
+        variant="bordered"
+        aria-label="Disabled Options"
+      >
+        {tabs.map((tab) => {
+          return (
+            <Tab key={tab.key} title={tab.title}>
+              <Card>
+                <CardBody>{tab.description}</CardBody>
+
+                <Button
+                  isLoading={isLoading}
+                  color="primary"
+                  onClick={() => onSelect(tab.key)}
+                  variant="solid"
+                  className="mt-1 mx-2"
+                >
+                  Start conversation
+                </Button>
+              </Card>
+            </Tab>
+          );
+        })}
+      </Tabs>
+    </div>
+  );
+};
+
+export default ChatSelectTabs;
