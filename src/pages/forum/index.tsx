6d86f25089e8b1f8451a6a2913c3994bf1e33531
Matěj Pešl <matejpesl1@gmail.com>
2023-10-26 16:19:50 +0200

    improve ui

--- src/pages/forum/index.tsx
+++ src/pages/forum/index.tsx
@@ -1,5 +1,5 @@
 import { Trans, t } from "@lingui/macro";
-import { Button } from "@nextui-org/react";
+import { Button, Spacer } from "@nextui-org/react";
 import { useState } from "react";
 import { FaPlus } from "react-icons/fa";
 import { CreateForumPostModal } from "@/components/forum/CreateForumPostModal";
@@ -7,6 +7,7 @@ import { ForumPostHighlight } from "@/components/forum/ForumPostHighlight";
 import { PinnedPost } from "@/components/forum/PinnedPost";
 import Page from "@/components/Page";
 import { api } from "@/lib/api";
+import Title from "@components/ui/Title";
 
 export default function ForumPage() {
   const [isCreatePostOpen, setIsCreatePostOpen] = useState(false);
@@ -16,41 +17,42 @@ export default function ForumPage() {
 
   return (
     <Page title={t`Forum`} unprotected>
-      <h1 className="text-xl font-bold">
+      <Title>
         <Trans>Pinned posts</Trans>
-      </h1>
+      </Title>
       {posts.isLoading || !posts.data ? (
         <p>
           <Trans>Loading...</Trans>
         </p>
       ) : (
-        <div className="flex flex-col gap-2 mt-5">
+        <div className="flex flex-col gap-2">
           {posts.data.map((item) => {
             return <PinnedPost key={item.id} {...item} />;
           })}
         </div>
       )}
 
-      <div className="flex flex-row gap-2 mt-7">
-        <h1 className="text-xl font-bold">
-          <Trans>Recent posts</Trans>
-        </h1>
+      <Spacer y={6} />
+
+      <Title className={"justify-between"}>
+        <Trans>Recent</Trans>
         <Button
           onClick={toggleCreatePostOpen}
           variant="bordered"
           size="md"
-          className="w-fit mx-auto mr-0"
+          className="ml-auto"
+          startContent={<FaPlus />}
         >
-          <FaPlus />
           <Trans>Create a new post</Trans>
         </Button>
-      </div>
+      </Title>
+
       {posts.isLoading || !posts.data ? (
         <p>
           <Trans>Loading...</Trans>
         </p>
       ) : (
-        <div className="flex flex-col gap-2 mt-5">
+        <div className="flex flex-col gap-2">
           {posts.data.map((item) => {
             return <ForumPostHighlight key={item.id} {...item} />;
           })}
