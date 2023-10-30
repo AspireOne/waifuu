5d839a88742abd838c64ad0d977146e180d81914
Matěj Pešl <matejpesl1@gmail.com>
2023-10-30 19:00:38 +0100

    feat: update to next 14

--- next.config.mjs
+++ next.config.mjs
@@ -8,6 +8,11 @@ import {withSentryConfig} from "@sentry/nextjs";
 
 /** @type {import("next").NextConfig} */
 const config = {
+    output: !!process.env.NEXT_PUBLIC_BUILDING_NATIVE ? "export" : undefined,
+    images: {
+        // Capacitor needs static export/build -> that doesn't support optimized images -> disable it so that Capacitor works.
+        unoptimized: !!process.env.NEXT_PUBLIC_BUILDING_NATIVE ? true : undefined,
+    },
     reactStrictMode: true,
     experimental: {
         swcPlugins: [
@@ -24,64 +29,75 @@ const config = {
         // The API routes can be instrumented/wrapped manually.
         autoInstrumentServerFunctions: false,
     },
-        async headers() {
-        return [{
-            // matching all API routes
-            source: "/api/:path*",
-            headers: [{key: "Access-Control-Allow-Credentials", value: "true"}, {
-                key: "Access-Control-Allow-Origin",
-                value: "http://localhost"
-            }, {key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT"}, {
-                key: "Access-Control-Allow-Headers",
-                value: "Origin, X-CSRF-Token, X-Requested-With, Accept, Accept-Version, authorization, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version"
-            },]
-        }]
+
+  // To allow cors.
+    async headers() {
+        return [
+            {
+                // matching all API routes
+                source: "/api/:path*",
+                headers: [
+                    {key: "Access-Control-Allow-Credentials", value: "true"},
+                    {
+                        key: "Access-Control-Allow-Origin",
+                        value: "http://localhost",
+                    },
+                    {
+                        key: "Access-Control-Allow-Methods",
+                        value: "GET,OPTIONS,PATCH,DELETE,POST,PUT",
+                    },
+                    {
+                        key: "Access-Control-Allow-Headers",
+                        value:
+                            "Origin, X-CSRF-Token, X-Requested-With, Accept, Accept-Version, authorization, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
+                    },
+                ],
+            },
+        ];
     },
 
-    images: {
-        // Capacitor needs static export/build -> that doesn't support optimized images -> disable it so that Capacitor works.
-        // TODO: Flip this back.
-        unoptimized: true,
-    },
-
-    /*    // !TODO: Remove this!!!
-        webpack(webpackConfig) {
-            return {
-                ...webpackConfig,
-                optimization: {
-                    minimize: false
-                }
-            };
-        }*/
+    /*
+          webpack(webpackConfig) {
+              return {
+                  ...webpackConfig,
+                  optimization: {
+                      minimize: false
+                  }
+              };
+          }*/
 };
 
 // '@ionic/react', '@ionic/core', '@stencil/core',
 //const withTM = next_transpile_modules([]);
 
-export default withSentryConfig(config, {
-// For all available options, see:
-// https://github.com/getsentry/sentry-webpack-plugin#options
+export default withSentryConfig(
+    config,
+    {
+        // For all available options, see:
+        // https://github.com/getsentry/sentry-webpack-plugin#options
 
-// Suppresses source map uploading logs during build
-    silent: true,
-    org: "companion-ns",
-    project: "javascript-nextjs",
-}, {
-// For all available options, see:
-// https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
+        // Suppresses source map uploading logs during build
+        silent: true,
+        org: "companion-ns",
+        project: "javascript-nextjs",
+    },
+    {
+        // For all available options, see:
+        // https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
 
-// Upload a larger set of source maps for prettier stack traces (increases build time)
-    widenClientFileUpload: true,
+        // Upload a larger set of source maps for prettier stack traces (increases build time)
+        widenClientFileUpload: true,
 
-// Transpiles SDK to be compatible with IE11 (increases bundle size)
-    transpileClientSDK: false,
+        // Transpiles SDK to be compatible with IE11 (increases bundle size)
+        transpileClientSDK: false,
 
-// Routes browser requests to Sentry through a Next.js rewrite to circumvent ad-blockers (increases server load)
-// tunnelRoute: "/monitoring",
+        // Routes browser requests to Sentry through a Next.js rewrite to circumvent ad-blockers (increases server load)
+        // tunnelRoute: "/monitoring",
 
-// Hides source maps from generated client bundles
-    hideSourceMaps: true,
+        // Hides source maps from generated client bundles
+        hideSourceMaps: true,
 
-// Automatically tree-shake Sentry logger statements to reduce bundle size
-    disableLogger: true,
-});
+        // Automatically tree-shake Sentry logger statements to reduce bundle size
+        disableLogger: true,
+    },
+);
