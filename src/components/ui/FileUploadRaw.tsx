27bd1230db6e0676f111fff20d97c3eab6cd6705
Matěj Pešl <matejpesl1@gmail.com>
2023-10-30 19:23:56 +0100

    refactor: rename api-routes to services

--- src/components/ui/FileUploadRaw.tsx
+++ src/components/ui/FileUploadRaw.tsx
@@ -1,5 +1,5 @@
 import React, { ChangeEvent, useState } from "react";
-import { apiPostImage } from "@/api-routes/apiImages";
+import { apiPostImage } from "@services/imageService";
 import { Trans } from "@lingui/macro";
 
 type FileUploadRawProps = {
