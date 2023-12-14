77004dff643f31273c82876bee9ba5dafb359d60
Matěj Pešl <matejpesl1@gmail.com>
2023-12-14 11:08:35 +0100

    add max messages

--- src/server/helpers/helpers.ts
+++ src/server/helpers/helpers.ts
@@ -1,1 +1,10 @@
-export const tokensToMessages = (tokens: number) => tokens / 140;
+const MAX_MESSAGES = 100; // 32k can handle up to 280 messages
+const ratio = 140;
+
+export const tokensToMessages = (tokens: number) => {
+  let value = tokens / ratio;
+  if (value > MAX_MESSAGES) {
+    value = MAX_MESSAGES;
+  }
+  return value;
+};
