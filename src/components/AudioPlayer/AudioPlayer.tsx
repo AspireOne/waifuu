745a51c4dbbedcc872fcc80e912098ec3eb6ce78
Jakub Dorničák <jakub.dornicak@blindspot.ai>
2024-02-05 14:23:21 +0100

    feat: 🎸 audio player working

--- src/components/AudioPlayer/AudioPlayer.tsx
+++ src/components/AudioPlayer/AudioPlayer.tsx
@@ -1,12 +1,28 @@
+import React, { useRef, useEffect, useState } from "react";
+import { HiMiniSpeakerWave, HiMiniSpeakerXMark } from "react-icons/hi2";
+import ReactAudioPlayer from "react-audio-player";
 import { Mood } from "@prisma/client";
-import React, { useRef, useEffect } from "react";
 
 interface AudioPlayerProps {
   mood: Mood;
 }
 
 const AudioPlayer: React.FC<AudioPlayerProps> = ({ mood }) => {
-  const audioRef = useRef<HTMLAudioElement>(null);
+  const audioPlayerRef = useRef<ReactAudioPlayer>(null);
+  const [isPlaying, setIsPlaying] = useState(false);
+
+  const togglePlaying = () => {
+    setIsPlaying((prev) => !prev);
+  };
+
+  useEffect(() => {
+    if (!isPlaying) {
+      audioPlayerRef.current?.updateVolume(0);
+    } else {
+      audioPlayerRef.current?.updateVolume(1);
+      audioPlayerRef.current?.clearListenTrack();
+    }
+  }, [isPlaying]);
 
   const tracks: { mood: Mood; src: string }[] = [
     {
@@ -27,16 +43,23 @@ const AudioPlayer: React.FC<AudioPlayerProps> = ({ mood }) => {
     },
   ];
 
-  useEffect(() => {
-    if (audioRef.current) {
-      audioRef.current.src =
-        tracks.find((item) => item.mood === mood)?.src ?? "";
-      audioRef.current.load();
-      audioRef.current.play();
-    }
-  }, [mood]);
-
-  return <audio loop ref={audioRef} />;
+  return (
+    <>
+      <ReactAudioPlayer
+        src={tracks.find((item) => item.mood === mood)?.src ?? ""}
+        ref={audioPlayerRef}
+        autoPlay
+        loop
+      />
+      <button className="fixed top-0 right-2 z-[50]" onClick={togglePlaying}>
+        {isPlaying ? (
+          <HiMiniSpeakerWave color="white" className="w-[50px] h-[50px]" />
+        ) : (
+          <HiMiniSpeakerXMark color="white" className="w-[50px] h-[50px]" />
+        )}
+      </button>
+    </>
+  );
 };
 
 export default AudioPlayer;
