1990f15f0faa00e4eb1f3e2ef9b5d0fc6df92016
Matěj Pešl <matejpesl1@gmail.com>
2024-03-10 02:27:14 +0100

    feat: use env variables in pusher server

--- src/server/clients/pusherServer.ts
+++ src/server/clients/pusherServer.ts
@@ -3,11 +3,12 @@ import PusherServer from "pusher";
 
 // biome-ignore lint/suspicious/noAssignInExpressions: off.
 export default (global.pusher ??= new PusherServer({
-  appId: "app-id",
-  key: "app-key",
-  secret: "app-secret",
-  //cluster: "EU",
+  appId: process.env.NEXT_PUBLIC_PUSHER_APP_ID as string,
+  key: process.env.NEXT_PUBLIC_PUSHER_APP_KEY as string,
+  secret: process.env.PUSHER_SECRET as string,
   host: process.env.NEXT_PUBLIC_PUSHER_HOST as string,
-  port: process.env.NEXT_PUBLIC_PUSHER_PORT,
-  useTLS: false, // idk? TODO: SOCKETI_SSL_CERT? https://docs.soketi.app/getting-started/ssl-configuration
+  port: process.env.NEXT_PUBLIC_PUSHER_PORT as string,
+  useTLS: true,
+  //encrypted: true,
+  cluster: "eu",
 }));
